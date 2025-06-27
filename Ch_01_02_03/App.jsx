8937dc1ce51db57422e6d05f4733dcf138b838c9
Tuhin Sengupta <senguptatuhin2222001@gmail.com>
2025-06-27 13:09:56 +0530

    Add files via upload
    Day 01/02/03 : Files are committed.

---
+++ Ch_01_02_03/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import One from "./Components/One.jsx";
+function App() {
+  return <div>
+    <One/>
+  </div>;
+}
+
+export default App;
---
+++ Ch_01_02_03/One.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function One() {
+  const name = "Tuhin";
+  const age = 24;
+  return (
+    <div>
+      <h1>
+        Welcome back {name} in this React course , you are doing great at the
+        age of {age}
+      </h1>
+    </div>
+  );
+}
+
+export default One;
