2879a5414384292de3137c734bb84c17c15c1cb2
Tuhin Sengupta <senguptatuhin2222001@gmail.com>
2025-06-28 17:34:47 +0530

    Add files via upload
    Day6 & 7(basic) : Files are committed

---
+++ Ch_6(theory)_Ch_7(props)/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import File from './Components/File.jsx'
+function App() {
+  return (
+    <div>
+      <File username="Gairik" password="Gai@222201"/>
+    </div>
+  )
+}
+
+export default App;
---
+++ Ch_6(theory)_Ch_7(props)/File.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App({username, password}) {
+  return (
+    <div>
+        <h1>My name is : {username}</h1>
+        <h2>My password is : {password}</h2>
+    </div>
+  )
+}
+
+export default App;
